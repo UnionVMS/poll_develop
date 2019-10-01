813a7c8cb144c6d82f14eff6e4cc299bd14743f2
thomasfanto <thomas.fanto@consid.se>
2019-10-01 14:34:02 +0200

    testing polls

--- src/main/java/dev/Dev.java
+++ src/main/java/dev/Dev.java
@@ -1,59 +1,122 @@
 package dev;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.net.Socket;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class Dev {
-	private static Logger LOGGER = LoggerFactory.getLogger(Dev.class);
-	private Functions functions = new Functions();
+    private static Logger LOGGER = LoggerFactory.getLogger(Dev.class);
+    private Functions functions = new Functions();
 
-	
-	private void developConfigPolls(BufferedInputStream input, PrintStream output) {
-		// TODO Auto-generated method stub
-		
+    String DNID = "10745";
+
+    private void trace(String str){
+		System.out.println(str);
 	}
 
-	private void go() {
-		Socket socket = null;
-		PrintStream output = null;
-		try {
-			socket = new Socket("148.122.32.20", 23);
-			// logon
-			BufferedInputStream input = new BufferedInputStream(socket.getInputStream());
-			output = new PrintStream(socket.getOutputStream());
-			functions.readUntil("name:", input);
-			functions.write("E32886SE", output);
-			functions.readUntil("word:", input);
-			functions.sendPwd(output, "4557");
-			functions.readUntil(">", input);
-			LOGGER.info("Logged in");
-			developConfigPolls(input, output);
-		} catch (Throwable t) {
-			LOGGER.error(t.toString(), t);
-		} finally {
-			if (output != null) {
-				output.print("QUIT \r\n");
-				output.flush();
-				LOGGER.info("Logged out");
-			}
-			if ((socket != null) && (socket.isConnected())) {
-				try {
-					socket.close();
-				} catch (IOException e) {
-					// OK
-				}
-			}
-		}
+	public void write(String value, PrintStream out) {
+		out.println(value);
+		out.flush();
 	}
 
-	public static void main(String[] args) {
-		Dev obj = new Dev();
-		obj.go();
-	}
+
+
+
+
+	private void stopPoll(BufferedInputStream input, PrintStream out) {
+		trace("stopPoll BEGIN");
+        String cmd = String.format("poll 0,G,%s,N,1,0,6", DNID);
+        try {
+            write(cmd, out);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+		trace("stopPoll END");
+    }
+
+
+    private void configPoll(BufferedInputStream input, PrintStream out) {
+		trace("configPoll BEGIN");
+
+        String cmd = String.format("poll 0,G,%s,N,1,0,4,,5611,24", DNID);
+
+        trace(cmd);
+        try {
+            write(cmd, out);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+		trace("configPoll END");
+    }
+
+    private void startPoll(BufferedInputStream input, PrintStream out) {
+		trace("startPoll BEGIN");
+
+        String cmd = String.format("poll 0,G,%s,D,1,0,5", DNID);
+        trace(cmd);
+        try {
+            write(cmd, out);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+		trace("startPoll END");
+    }
+
+
+    private void developConfigPolls(BufferedInputStream input, PrintStream output) {
+
+		stopPoll(input, output);
+        configPoll(input, output);
+        startPoll(input, output);
+        //stopPoll(input, output);
+
+
+		trace("Ready");
+
+
+
+    }
+
+    private void go() {
+        Socket socket = null;
+        PrintStream output = null;
+        try {
+            socket = new Socket("148.122.32.20", 23);
+            // logon
+            BufferedInputStream input = new BufferedInputStream(socket.getInputStream());
+            output = new PrintStream(socket.getOutputStream());
+            functions.readUntil("name:", input);
+            functions.write("E32886SE", output);
+            functions.readUntil("word:", input);
+            functions.sendPwd(output, "4557");
+            functions.readUntil(">", input);
+            LOGGER.info("Logged in");
+            developConfigPolls(input, output);
+        } catch (Throwable t) {
+            LOGGER.error(t.toString(), t);
+        } finally {
+            if (output != null) {
+                output.print("QUIT \r\n");
+                output.flush();
+                LOGGER.info("Logged out");
+            }
+            if ((socket != null) && (socket.isConnected())) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    // OK
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Dev obj = new Dev();
+        obj.go();
+    }
 }
